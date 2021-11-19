2597a67fa2fa7542fe7fe1531ea131c5c5c4e77e
yigityalin <yigityalin02@gmail.com>
2021-11-19 19:53:53 +0300

    Update OneCycleScheduler.py

--- OneCycleScheduler.py
+++ OneCycleScheduler.py
@@ -7,7 +7,7 @@ class OneCycleScheduler(keras.callbacks.Callback):
                min_lr, max_lr, 
                min_momentum, max_momentum,
                annealing_phase, annealing_rate):
-
+    super(OneCycleScheduler, self).__init__()
     self.total_iterations = total_iterations
     self.min_lr = min_lr
     self.max_lr = max_lr
@@ -18,7 +18,6 @@ class OneCycleScheduler(keras.callbacks.Callback):
     self.min_annealing_lr = min_lr * annealing_rate
     self.lr_peak = int(total_iterations * (1 - annealing_phase) / 2)
     self.iterations = 0
-    self.history = {}
   
   def on_train_begin(self, logs=None):
     logs = logs or {}
@@ -28,13 +27,6 @@ class OneCycleScheduler(keras.callbacks.Callback):
   def on_batch_end(self, batch, logs=None):
     logs = logs or {}
     self.iterations += 1
-
-    self.history.setdefault("lr", []).append(K.get_value(self.model.optimizer.lr))
-    self.history.setdefault("momentum", []).append(K.get_value(self.model.optimizer.momentum))
-    self.history.setdefault("iterations", []).append(K.get_value(self.iterations))
-
-    for k, v in logs.items():
-      self.history.setdefault(k, []).append(v)
 
     K.set_value(self.model.optimizer.lr, self._calculate_lr())
     K.set_value(self.model.optimizer.momentum, self._calculate_lr())
